f513b87244c35e7e43ca60e7b567175a94586cfc
zendhuzhengdong_i <zhangwenqiang_>
2018-04-22 21:35:12 +0800

    servlet fix

--- src/main/java/co/hzdmm123/c1/controller/CustomerCreateServlet.java
+++ src/main/java/co/hzdmm123/c1/controller/CustomerCreateServlet.java
@@ -17,6 +17,7 @@ public class CustomerCreateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
+        
     }
 
     //处理客户请求
